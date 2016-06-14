d4f59d71266fd58ba4a2528969cb1f55a75ba695
Rafał Byczek <rafal.algo@gmail.com>
2016-06-14 23:57:28 +0200

    podpowiedzi v2

--- src/kalambury/model/Password.java
+++ src/kalambury/model/Password.java
@@ -1,35 +1,25 @@
 package kalambury.model;
+import kalambury.database.Database;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
-/**
- * Created by Michal Stobierski on 2016-06-08.
- */
-
 public class Password {
-
-
     public static Random rNum = new Random();
     public static List<String> slowa = new ArrayList<>();
     public static Map<String, MyPair> slownik = new HashMap<>();
 
     static {
-
         final Path file = Paths.get("src/kalambury/model/slownik.txt");
         final Charset charset = Charset.forName("UTF-8");
         try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
-
             String line = reader.readLine();
-            int n = Integer.parseInt(line);
-
-            while (n-- > 0) {
-                line = reader.readLine();
-
+            while (line != null) {
                 String[] data = line.split("[|]");
                 List<String> tmpList = new ArrayList<>();
                 int tmpIt = 0;
@@ -39,8 +29,10 @@ public class Password {
                 }
                 MyPair noweHaslo = new MyPair(tmpList);
                 slownik.put(data[0], noweHaslo);
+                slowa.add(data[0]);
+                line = reader.readLine();
             }
-
+            reader.close();
         } catch (final IOException x) {
             System.err.format("Nie znaleziono pliku z danymi - IOException: %s%n", x);
         }
@@ -48,7 +40,9 @@ public class Password {
     }
 
     public static String getWord(String word) {
-        return slowa.get(rNum.nextInt(slowa.size()));
+        Database.instance.changeWord("DELETE FROM slowo WHERE slowo.slowo = '" + word + "'");
+        Database.instance.changeWord("INSERT INTO slowo(slowo) VALUES('" + slowa.get(rNum.nextInt(slowa.size())) + "')");
+        return Database.instance.getWord("SELECT slowo FROM slowo LIMIT 1;");
     }
 
     public static String getHint(String haslo) {
